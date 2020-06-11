0988544c6dfc3fe2dba18c9f07f0bf2ffcc2e41b
Delusion-joker <cjydota@outlook.com>
2020-06-11 20:08:00 +0800

    init gshop

---
+++ src/components/ShopList/ShopList.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="shop_container">
+            <ul class="shop_list">
+              <li class="shop_li border-1px">
+                <a>
+                  <div class="shop_left">
+                    <img class="shop_img" src="./images/shop/1.jpg">
+                  </div>
+                  <div class="shop_right">
+                    <section class="shop_detail_header">
+                      <h4 class="shop_title ellipsis">锄禾日当午，汗滴禾下土</h4>
+                      <ul class="shop_detail_ul">
+                        <li class="supports">保</li>
+                        <li class="supports">准</li>
+                        <li class="supports">票</li>
+                      </ul>
+                    </section>
+                    <section class="shop_rating_order">
+                      <section class="shop_rating_order_left">
+                        <div class="star star-24">
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item half"></span>
+                          <span class="star-item off"></span>
+                        </div>
+                        <div class="rating_section">
+                          3.6
+                        </div>
+                        <div class="order_section">
+                          月售106单
+                        </div>
+                      </section>
+                      <section class="shop_rating_order_right">
+                        <span class="delivery_style delivery_right">硅谷专送</span>
+                      </section>
+                    </section>
+                    <section class="shop_distance">
+                      <p class="shop_delivery_msg">
+                        <span>¥20起送</span>
+                        <span class="segmentation">/</span>
+                        <span>配送费约¥5</span>
+                      </p>
+                    </section>
+                  </div>
+                </a>
+              </li>
+              <li class="shop_li border-1px">
+                <a>
+                  <div class="shop_left">
+                    <img class="shop_img" src="./images/shop/2.jpg">
+                  </div>
+                  <div class="shop_right">
+                    <section class="shop_detail_header">
+                      <h4 class="shop_title ellipsis">锄禾日当午，汗滴禾下土</h4>
+                      <ul class="shop_detail_ul">
+                        <li class="supports">保</li>
+                        <li class="supports">准</li>
+                        <li class="supports">票</li>
+                      </ul>
+                    </section>
+                    <section class="shop_rating_order">
+                      <section class="shop_rating_order_left">
+                        <div class="star star-24">
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item off"></span>
+                        </div>
+                        <div class="rating_section">
+                          4.1
+                        </div>
+                        <div class="order_section">
+                          月售106单
+                        </div>
+                      </section>
+                      <section class="shop_rating_order_right">
+                        <span class="delivery_style delivery_right">硅谷专送</span>
+                      </section>
+                    </section>
+                    <section class="shop_distance">
+                      <p class="shop_delivery_msg">
+                        <span>¥20起送</span>
+                        <span class="segmentation">/</span>
+                        <span>配送费约¥5</span>
+                      </p>
+                    </section>
+                  </div>
+                </a>
+              </li>
+              <li class="shop_li border-1px">
+                <a>
+                  <div class="shop_left">
+                    <img class="shop_img" src="./images/shop/3.jpg">
+                  </div>
+                  <div class="shop_right">
+                    <section class="shop_detail_header">
+                      <h4 class="shop_title ellipsis">锄禾日当午，汗滴禾下土</h4>
+                      <ul class="shop_detail_ul">
+                        <li class="supports">保</li>
+                        <li class="supports">准</li>
+                        <li class="supports">票</li>
+                      </ul>
+                    </section>
+                    <section class="shop_rating_order">
+                      <section class="shop_rating_order_left">
+                        <div class="star star-24">
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item off"></span>
+                          <span class="star-item off"></span>
+                        </div>
+                        <div class="rating_section">
+                          3.2
+                        </div>
+                        <div class="order_section">
+                          月售106单
+                        </div>
+                      </section>
+                      <section class="shop_rating_order_right">
+                        <span class="delivery_style delivery_right">硅谷专送</span>
+                      </section>
+                    </section>
+                    <section class="shop_distance">
+                      <p class="shop_delivery_msg">
+                        <span>¥20起送</span>
+                        <span class="segmentation">/</span>
+                        <span>配送费约¥5</span>
+                      </p>
+                    </section>
+                  </div>
+                </a>
+              </li>
+              <li class="shop_li border-1px">
+                <a>
+                  <div class="shop_left">
+                    <img class="shop_img" src="./images/shop/4.jpg">
+                  </div>
+                  <div class="shop_right">
+                    <section class="shop_detail_header">
+                      <h4 class="shop_title ellipsis">锄禾日当午，汗滴禾下土</h4>
+                      <ul class="shop_detail_ul">
+                        <li class="supports">保</li>
+                        <li class="supports">准</li>
+                        <li class="supports">票</li>
+                      </ul>
+                    </section>
+                    <section class="shop_rating_order">
+                      <section class="shop_rating_order_left">
+                        <div class="star star-24">
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item on"></span>
+                          <span class="star-item half"></span>
+                          <span class="star-item off"></span>
+                        </div>
+                        <div class="rating_section">
+                          3.6
+                        </div>
+                        <div class="order_section">
+                          月售106单
+                        </div>
+                      </section>
+                      <section class="shop_rating_order_right">
+                        <span class="delivery_style delivery_right">硅谷专送</span>
+                      </section>
+                    </section>
+                    <section class="shop_distance">
+                      <p class="shop_delivery_msg">
+                        <span>¥20起送</span>
+                        <span class="segmentation">/</span>
+                        <span>配送费约¥5</span>
+                      </p>
+                    </section>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+</template>
+<script>
+ export default{}
+</script>
+<style lang='stylus' rel='stylesheet/stylus'>
+    @import '../../common/stylus/mixins.styl'
+            .swiper-container
+              width 100%
+              height 100%
+              .swiper-wrapper
+                width 100%
+                height 100%
+                .swiper-slide
+                  display flex
+                  justify-content center
+                  align-items flex-start
+                  flex-wrap wrap
+                  .link_to_food
+                    width 25%
+                    .food_container
+                      display block
+                      width 100%
+                      text-align center
+                      padding-bottom 10px
+                      font-size 0
+                      img
+                        display inline-block
+                        width 50px
+                        height 50px
+                    span
+                      display block
+                      width 100%
+                      text-align center
+                      font-size 13px
+                      color #666
+              .swiper-pagination
+                >span.swiper-pagination-bullet-active
+                  background #02a774
+</style>
